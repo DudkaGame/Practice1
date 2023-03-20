e34fe7de527234e819bdceb7cbecb40dbceefd0d
DudkaGame <dimaponomar2019@gmail.com>
2023-03-21 00:50:02 +0300

    Обновил базу

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,7 +22,6 @@ bool flagChange = 0; // статус изменений
 QByteArray ba; // массив
 QMessageBox::StandardButton reply; //ответ пользователя да/нет/отмена
 QString fileName;
-#define TITLE_STRING "*Без Имени - NotePad  ©  ПОНОМАРЕВ ДИМИТРИЙ "
 
 // __________________________    ОПИСЫВАЕМ БЛОК ФУНКЦИОНИРОВАНИЯ
 
@@ -32,17 +31,16 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::open(){ //функция открыть
-    flagSave = 1; flagChange = 0;
 
     QString fileName; // путь к документу
     fileName = QFileDialog::getOpenFileName(this, \
     tr("Открыть файл"), "D:\\Work\\Study\\Programming\\Qt_worked\\2_sem\\1_practice\\Doci", tr("Text files(*.txt)"));
-    //задаем по умолчанию расположение и фильтруем по расширению
 
     if (fileName == ""){
-        QMessageBox::information(this, "Предупреждение", "Файл не выбран");
+        QMessageBox::information(this, "Предупреждение", "Файл не выбран"); return;
     }
     else {
+        flagSave = 1; flagChange = 0;
         ui->textEdit->clear(); // очищаем текстовое поле
         QFile file; // класс файлов
         file.setFileName(fileName); // связываем с файлом на диске
@@ -59,33 +57,32 @@ void MainWindow::open(){ //функция открыть
 
 void MainWindow::New(){ // функция создать
     flagSave = 0; flagChange = 0;
-    ba.clear(); ui->textEdit->clear(); return;
+    ba.clear(); ui->textEdit->clear(); this->setWindowTitle("NotePad  ©  ПОНОМАРЕВ ДИМИТРИЙ"); return;
 }
 
 void MainWindow::save(){ // функция сохраниить
-    flagSave = 1; flagChange = 0;
-
+    flagSave = 1; flagSave = 0;
     QString fileName; // путь к документу
     QFile file;
     file.setFileName(fileName); //связываем с файлом на диске
     file.open(QIODevice::WriteOnly); //открываем файл на запись
-    file.write(ui->textEdit->toPlainText().toUtf8()); //Запись информации с текстового поля в файд
+    file.write(ui->textEdit->toPlainText().toUtf8()); //Запись информации с текстового поля в файл
     file.close(); //закрываем файл
 
 }
 
 void MainWindow::saveAs(){ // функция сохранить как
-    flagSave = 1; flagChange = 0;
-
     QString fileName; // путь к документу
     fileName = QFileDialog::getSaveFileName(this, tr("Сохранить как"), \
     "D:\\Work\\Study\\Programming\\Qt_worked\\2_sem\\1_practice\\Doci", tr("Text files (*.txt)"));
+
     if (fileName.isEmpty()) //Файл не выбран
     {
         QMessageBox::information(this, "Предупреждение", "Файл не выбран");
     }
     else
     {
+        flagSave = 1; flagChange = 0;
         QFile file;
         file.setFileName(fileName); //связываем с файлом на диске
         file.open(QIODevice::WriteOnly); //открываем файл на запись
@@ -97,14 +94,25 @@ void MainWindow::saveAs(){ // функция сохранить как
 
 void MainWindow::on_textEdit_textChanged() // изменения
 {
-    flagChange = 1;
     debug();
+    flagChange = 1;
+
+//    ui->lineEdit_3->setText(fileName);
+//    if(!fileName.isEmpty()){
+//        QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), QDir::homePath(), tr("Text Files (*.txt);;All Files (*)"));
+//        QFileInfo fileInfo(fileName);
+//        QString name = fileInfo.fileName();
+//        this->setWindowTitle(name + TITLE_STRING);
+//    }
+//    else{
+//        this->setWindowTitle(TITLE_STRING);
+//   }
 
 }
 
 void MainWindow::debug(){// дебаг
-    QString TflagSave, TflagChange = "";
 
+    QString TflagSave, TflagChange = "";
     TflagSave = flagSave ? "true" : "false";
     TflagChange = flagChange? "true" : "false";
     ui->lineEdit->setText(TflagSave);
@@ -120,15 +128,14 @@ void MainWindow::on_pushButton_clicked()
 // __________________________ ОПИСЫВАЕМ БЛОК ИНТЕРФЕЙСА
 
 void MainWindow::on_new_d_triggered(){ //кнопка new
-//    QFileDialog dialog(this);
-//        dialog.setFileMode(QFileDialog::ExistingFile);
+    QFileDialog dialog(this);
+        dialog.setFileMode(QFileDialog::ExistingFile);
     if (flagSave && flagChange){ // сохранен -> изменен
         reply = QMessageBox::question(this, "Предупреждение",
             "Вы желаете сохраниться?",
             QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
-
         if (reply == QMessageBox::Yes){
-            save(); New(); return;
+            save(); New();return;
             }
         if (reply == QMessageBox::No){
             New(); return;
@@ -136,18 +143,21 @@ void MainWindow::on_new_d_triggered(){ //кнопка new
         if (reply == QMessageBox::Cancel){
             return;}
     }
-    if ((flagSave && !flagChange) || (!flagSave && !flagChange)){ // сохранен -> не изменен || не сохранен -> не изменен
-        New();return;
+    if ((flagSave && !flagChange) || (!flagSave && !flagChange)){ // сохранен -> не изменен
+        New(); return; //сделать название
     }
-    if (!flagSave && flagChange){ //  не сохранен -> не изменен
+    if (!flagSave && flagChange){ // сохранен -> не изменен
+//        ui->lineEdit->setText("3");
         reply = QMessageBox::question(this, "Предупреждение",
             "Вы желаете сохраниться?",
             QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
         if (reply == QMessageBox::Yes){
-            saveAs(); New(); return;
+            saveAs(); New(); flagSave = 0; return;
+            //сделать название
             }
         if (reply == QMessageBox::No){
-            New(); return;
+            New(); flagSave = 0; flagChange = 0; return;
+            //сделать название
             }
         if (reply == QMessageBox::Cancel){
             return;}
@@ -160,15 +170,15 @@ void MainWindow::on_open_triggered(){ //кнопка open
             "Вы желаете сохраниться?",
             QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
         if (reply == QMessageBox::Yes){
-            save(); open(); flagSave = 1; flagChange = 0;  return;
+            save(); open(); return;
             }
         if (reply == QMessageBox::No){
-            open(); flagSave = 1; flagChange = 0;  return;
+            open(); return;
             }
         if (reply == QMessageBox::Cancel){
             return;}
     }
-    if ((flagSave && !flagChange) || (!flagSave && !flagChange)){ // сохранен -> не изменен сохранен -> не изменен || не сохранен -> не изменен
+    if ((flagSave && !flagChange) || (!flagSave && !flagChange)){ // сохранен -> не изменен или не сохранен -> не изменен
         open(); return;
     }
     if (!flagSave && flagChange){ // сохранен -> не изменен
@@ -187,12 +197,11 @@ void MainWindow::on_open_triggered(){ //кнопка open
 }
 
 void MainWindow::on_save_triggered(){ //кнопка сохранить
-
     if (flagSave && flagChange){ // сохранен -> изменен
         save(); return;
     }
-    if ((flagSave && !flagChange) || (!flagSave && !flagChange)){ // сохранен -> не изменен || не сохранен -> не изменен
-        return; //сделать название
+    if ((flagSave && !flagChange) || (!flagSave && !flagChange)){ // сохранен -> не изменен или не сохранен -> не изменен
+        return;
     }
     if (!flagSave && flagChange){ // не сохранен -> изменен
         saveAs(); return;
@@ -200,11 +209,10 @@ void MainWindow::on_save_triggered(){ //кнопка сохранить
 }
 
 void MainWindow::on_saveAs_triggered(){ //кнопка сохранить как
-    saveAs(); return;
+    saveAs();
 }
 
 void MainWindow::on_exit_triggered(){ // кнопка выхода
-
     if (flagSave && flagChange){ // сохранен -> изменен
         reply = QMessageBox::question(this, "Предупреждение",
             "Вы желаете сохраниться?",
@@ -218,7 +226,7 @@ void MainWindow::on_exit_triggered(){ // кнопка выхода
         if (reply == QMessageBox::Cancel){
             return;}
     }
-    if ((flagSave && !flagChange) || (!flagSave && !flagChange)){ //сохранен -> не изменен сохранен -> не изменен || не сохранен -> не изменен
+    if ((flagSave && !flagChange) || (!flagSave && !flagChange)){ // сохранен -> не изменен или не сохранен -> не изменен
         exit(0);
     }
     if (!flagSave && flagChange){ // сохранен -> не изменен
@@ -234,6 +242,7 @@ void MainWindow::on_exit_triggered(){ // кнопка выхода
         if (reply == QMessageBox::Cancel){
             return;}
     }
+
 }
 
 
